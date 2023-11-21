24d24b3752db7ec061e389aa87894af5ad439e6e
Allison <theallietang>
2023-11-21 19:50:06 +0800

    [BugFix - tailwind styles not reflecting] reinstall & import customized themes

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+
+// import { logo, sun } from '../assets';
+// import { navlinks } from '../constants';
+
+const Sidebar = () => {
+    // const navigate = useNavigate();
+    // const [isActive, setIsActive] = useState('dashboard')
+    return (
+        <div>
+             
+        </div>
+    )
+}
+
+export default Sidebar
